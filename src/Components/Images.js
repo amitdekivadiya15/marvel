294184c0d3857a0370f9ba410ea7b784932596b7
amitdekivadiya15 <amit.spyhunteritsolution@gmail.com>
2021-12-04 18:01:09 +0530

    cart

---
+++ src/Components/Images.js
@@ -0,0 +1,29 @@
+const Images = [
+  {
+    id: 1,
+    img: "images/fantasticfour_lob_crd_01.jpg",
+    title: "Fantastic Four",
+  },
+  {
+    id: 2,
+    img: "images/antman_lob_crd_01.jpg",
+    title: "Ant-Man",
+  },
+  {
+    id: 3,
+    img: "images/blade_lob_crd_01.jpg",
+    title: "Blade",
+  },
+  {
+    id: 4,
+    img: "images/themarvels_lob_crd_01.jpg",
+    title: "The Marvels",
+  },
+  {
+    id: 5,
+    img: "images/blackpanther2_lob_crd_02.jpg",
+    title: "Black Panther",
+  },
+];
+
+export default Images;
---
+++ src/Components/MainCart.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Card } from "antd";
+import Images from "./Images";
+const { Meta } = Card;
+
+const MainCart = (props) => {
+  const item = Images;
+  return (
+    <>
+      <div className="my-3">
+        <h3 style={{ textAlign: "center" }}>MARVEL MOVIES</h3>
+      </div>
+      <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+        {item.map((e) => {
+          const { id, img, title } = e;
+
+          return (
+            <div className="col" key={id}>
+              <Card
+                hoverable
+                style={{ width: "240px" }}
+                cover={<img alt="fantastic four" src={img} />}
+              >
+                <Meta title={title} />
+              </Card>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default MainCart;
